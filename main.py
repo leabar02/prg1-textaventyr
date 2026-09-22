03112618ee1a7a96176834e5e58623dfd33b5b99
Leander Bardt <leabar02@edu.umea.se>
2026-09-22 11:16:04 +0200

    tisdag lektion 11.15

--- main.py
+++ main.py
@@ -5,3 +5,32 @@ namn = input()
 print(f"Hej! {namn}")
 
 print("Explosion hörs i distansen, vad var det?")
+
+print("Kolla! En stor titan sparkade just ett hål i den stora väggen.")
+
+print("Vad ska vi göra nu?")
+
+input()
+
+print("Titan står 29 meter bort och..")
+titan = print("Kollar på dig")
+
+rädsla = input()
+print("Du är rädd")
+
+rädsla == print("Du springer hem!")
+
+
+print("När du kommer hem är ditt hus begravd av en stor sten.") 
+print("Din mamma ligger under stenen men lever.")
+
+input()
+input()
+
+print("Steg hörs som ett hårt dunns i marken.")
+print("En tiatn kommer närmare!")
+print("vad gör du?")
+
+if namn ("Stannar kvar och hjälper din mamma"):
+    print("Du kommer att dö")
+
